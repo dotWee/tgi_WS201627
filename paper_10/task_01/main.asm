133b3681c9f4085fb8f1991f896f6d207b40a358
Lukas Wolfsteiner <lukas.wolfsteiner@st.oth-regensburg.de>
2020-06-15 18:04:33 +0200

    Update solution for task 01 paper 10

--- paper_10/task_01/main.asm
+++ paper_10/task_01/main.asm
@@ -1,96 +1,102 @@
 %include "io.inc"
+
 section .data
-OPA DD 0h
-OPB DD 0h
-OPERAND DB 0h
-ERG DQ 0h
-ZEHN DD 10
+    ZAHL1       DD  0h
+    ZAHL2       DD  0h
+    OPERAND     DB  0h
+    ERGEBNIS    DQ  0h
+    ZEHN        DD  10
 
 section .text
-global CMAIN
+    global CMAIN
+
 CMAIN:
-    mov ebp, esp; for correct debugging
+    mov ebp, esp            ; for correct debugging
     GET_CHAR [OPERAND]
-    GET_DEC 4, [OPA]
-    GET_DEC 4, [OPB]
-    
+    GET_DEC 4, [ZAHL1]
+    GET_DEC 4, [ZAHL2]
+
     CMP BYTE[OPERAND], '+'
     JE Addition
+
     CMP BYTE[OPERAND], '-'
     JE Subtraktion
+
     CMP BYTE[OPERAND], '/'
     JE Division
+
     CMP BYTE[OPERAND], '*'
     JE Multiplikation
+
     CMP BYTE[OPERAND], '%'
     JE Modulo
+
     JMP Error
-    
-    Addition:
-    MOV EAX, [OPA]
-    ADD EAX, [OPB]
+
+Addition:
+    MOV EAX, [ZAHL1]
+    ADD EAX, [ZAHL2]
     JMP Ende
-    
-    Subtraktion:
-    MOV EAX, [OPA]
-    SUB EAX, [OPB]
+
+Subtraktion:
+    MOV EAX, [ZAHL1]
+    SUB EAX, [ZAHL2]
     JMP Ende
-    
-    Division:
-    MOV EAX, [OPA]
+
+Division:
+    MOV EAX, [ZAHL1]
     MOV EDX, 0
-    DIV DWORD[OPB]
+    DIV DWORD[ZAHL2]
     JMP Ende
-    
-    Modulo:
-    MOV EAX, [OPA]
+
+Modulo:
+    MOV EAX, [ZAHL1]
     MOV EDX, 0
-    DIV DWORD[OPB]
+    DIV DWORD[ZAHL2]
     MOV EAX, EDX
     JMP Ende
-    
-    Multiplikation:
-    MOV EAX, [OPA]
-    MUL DWORD[OPB]
+
+Multiplikation:
+    MOV EAX, [ZAHL1]
+    MUL DWORD[ZAHL2]
     CMP EDX, 0
     JNE AusgabeMultiplikationVor
     JMP Ende
-    
-    Ende:
+
+Ende:
     PRINT_DEC 4, EAX
     XOR EAX, EAX
     MOV ESP, EBP
     RET
-    
-    Error:
+
+Error:
     PRINT_STRING 'Fehler'
     RET
-    
-    AusgabeMultiplikationVor:
+
+AusgabeMultiplikationVor:
     MOV ECX, 0
-    MOV DWORD[ERG], EAX
-    MOV DWORD[ERG+4], EDX
-    
-    AusgabeMultiplikation:
+    MOV DWORD[ERGEBNIS], EAX
+    MOV DWORD[ERGEBNIS + 4], EDX
+
+AusgabeMultiplikation:
     XOR EDX, EDX
-    MOV EAX, [ERG+4]
+    MOV EAX, [ERGEBNIS + 4]
     DIV DWORD[ZEHN]
-    MOV [ERG+4], EAX
-    MOV EAX, [ERG]
+    MOV [ERGEBNIS + 4], EAX
+    MOV EAX, [ERGEBNIS]
     DIV DWORD[ZEHN]
-    MOV [ERG], EAX
+    MOV [ERGEBNIS], EAX
     PUSH EDX
     INC ECX
-    MOV EBX, [ERG]
-    ADD EBX, [ERG+4]
+    MOV EBX, [ERGEBNIS]
+    ADD EBX, [ERGEBNIS + 4]
     CMP EBX, 0
     JNE AusgabeMultiplikation
-    
-    Schleife:
+
+Schleife:
     POP EBX
     PRINT_HEX 4, EBX
     LOOP Schleife
-    
     XOR EAX, EAX
     MOV ESP, EBP
     RET
